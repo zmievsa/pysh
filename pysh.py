7d25b563fac85ca5f03418f531e42024cf02da3f
Ovsyanka83 <szmiev2000@gmail.com>
2022-03-19 18:13:56 +0300

    Move post- and pre- processing logic into import_from_path

--- pysh.py
+++ pysh.py
@@ -6,7 +6,6 @@ import sys
 import types
 import typing as T
 from pathlib import Path as P
-
 import typer
 
 __version__ = "0.2.0"
@@ -18,28 +17,50 @@ PIPE = subprocess.PIPE
 
 def sh(cmd: T.Union[str, T.List[str]], extra_env: T.Dict[str, str] = {}, **kwargs):
     kwargs["env"] = dict(**(kwargs.get("env") or os.environ), **extra_env)
-    if kwargs.get("input") is None:
-        kwargs["stdin"] = subprocess.PIPE
     return subprocess.run(cmd, shell=True, check=True, text=True, **kwargs)
 
 
-def import_from_path(module_name: str, path: P) -> types.ModuleType:
+def import_from_path(
+    module_name: str,
+    path: P,
+    preprocess: T.Optional[T.Callable[[bytes], bytes]] = None,
+    trim_traceback: bool = True,
+) -> types.ModuleType:
     spec = importlib.util.spec_from_file_location(module_name, path)
     if not (spec and spec.loader):
         raise TypeError(f"File loader for {path} was not found. Please, refer to importlib docs.")
     module = importlib.util.module_from_spec(spec)
     sys.modules[module_name] = module
-    # exec_module is not always available which is why a linter won't be able to find it.
-    # Docs do not state specific conditions for when it is available so if it's not, we
-    # are okay with raising an ImportError.
-    spec.loader.exec_module(module)  # CAN raise ImportError
+
+    try:
+        # A cute little trick to preprocess code before running it
+        if preprocess is not None:
+            original_source_to_code = spec.loader.source_to_code
+            def source_to_code(data: bytes, path, *, _optimize=-1):
+                return original_source_to_code(preprocess(data), path, _optimize=_optimize)
+            spec.loader.source_to_code = source_to_code
+            original_dont_write_bytecode = sys.dont_write_bytecode
+            sys.dont_write_bytecode = True
+            spec.loader.exec_module(module)
+            sys.dont_write_bytecode = original_dont_write_bytecode
+            spec.loader.source_to_code = original_source_to_code
+        else:
+            spec.loader.exec_module(module)
+    except SystemExit as e:
+        raise
+    except BaseException:
+        if not trim_traceback:
+            raise
+        import traceback
+
+        exc = traceback.format_exc()
+        if "_call_with_frames_removed\n" in exc:
+            exc = "Traceback (most recent call last):\n" + exc[exc.find("_call_with_frames_removed\n") + len("_call_with_frames_removed\n") :]
+        sys.stderr.write(exc)
+        raise
+
     return module
 
-
-def version_callback(show_version: bool):
-    if show_version:
-        typer.echo(__version__)
-        raise typer.Exit()
 
 
 def main(argv: T.Optional[T.List[str]] = None) -> None:
@@ -63,16 +84,10 @@ def main(argv: T.Optional[T.List[str]] = None) -> None:
         setattr(builtins, attr, globals_[attr])
     for index, arg in enumerate(argv):
         setattr(builtins, f"a{index}", arg)
+
     try:
         import_from_path(argv[0], P(argv[0]))
-    except SystemExit:
-        pass
-    except BaseException:
-        import traceback
-
-        exc = traceback.format_exc()
-        exc = exc[exc.find("_call_with_frames_removed\n") + len("_call_with_frames_removed\n") :]
-        sys.stderr.write("Traceback (most recent call last):\n" + exc)
+    except:
         exit(1)
 
 
